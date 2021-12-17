228f89af579453f134333d6d0410161996e5b81a
manucarenjot <carenjotje@gmail.com>
2021-12-17 10:16:22 +0100

    test ajax

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,24 @@
 const requestURL = "https://run.mocky.io/v3/c735a13f-b121-42c9-ae43-b4f7989b6b2b";
-
 let xhr = new XMLHttpRequest();
+xhr.open("GET",requestURL);
+xhr.response.type = "json";
 
-xhr.open("GET", requestURL);
-
-
-xhr.responseType = "json";
-
-xhr.onload = function() {
-    if (xhr.status !== 200) {
-        console.log('erreur');
+xhr.onload = function () {
+    if (xhr.status === 200) {
+        console.log("ok")
+        let response = xhr.response;
+        console.log(response);
+        let utilisateur = response.theme;
+        alert("Hello" + utilisateur.type + " " + utilisateur.fontSize);
     }
 }
 
-let response = xhr.response;
-console.log(response);
+xhr.onprogress = function (event) {
+    if (event.lengthComputable) {
+        let charge = event.loaded;
+        let total = event.total;
+        console.log(charge / 1024 + "kilo octets (ko) reçus sur " + (total / 1024) + "ko");
+    }
+}
 
 xhr.send();
